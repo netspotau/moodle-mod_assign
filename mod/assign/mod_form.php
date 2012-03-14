e525be5b6a7898062279e9d8cc575e954708ae83
Damyon Wiese <damyon.wiese@netspot.com.au>
2012-03-14 08:28:47 +0800

    MDL-31288: Merge bug with master

--- mod/assign/mod_form.php
+++ mod/assign/mod_form.php
@@ -83,6 +83,13 @@ class mod_assign_mod_form extends moodleform_mod {
         $mform->setDefault('submissiondrafts', 0);
         $mform->addElement('selectyesno', 'sendnotifications', get_string('sendnotifications', 'assign'));
         $mform->setDefault('sendnotifications', 1);
+        // submission statement
+        $config = get_config('assign');
+        if (!$config->require_submission_statement) {
+            $mform->addElement('select', 'requiresubmissionstatement', get_string('requiresubmissionstatement', 'ass
+            $mform->setDefault('requiresubmissionstatement', $config->submission_statement!='');
+        }
+
         
         // plagiarism enabling form
         plagiarism_get_form_elements_module($mform, $ctx->get_course_context());
