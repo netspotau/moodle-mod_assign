e3b2f53b1000707a4c97f7bedf667343b51c865d
Raymond Wijaya <raymond.wijaya@netspot.com.au>
2012-03-27 11:45:51 +1030

    MDL-31270: Replace all lines that used  'get_string' function with 'new lang_string' function in settings.php

--- mod/assign/settings.php
+++ mod/assign/settings.php
@@ -26,17 +26,14 @@
 defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->dirroot . '/mod/assign/adminlib.php');
-
+                                              
 $ADMIN->add('modules', new admin_category('assignmentplugins',
-        get_string('assignmentplugins', 'assign'), !$module->visible));
+         new lang_string('assignmentplugins', 'assign'), !$module->visible));
 $ADMIN->add('assignmentplugins', new admin_category('assignsubmissionplugins',
-        get_string('submissionplugins', 'assign'), !$module->visible));
-
+        new lang_string('submissionplugins', 'assign'), !$module->visible));
 $ADMIN->add('assignsubmissionplugins', new admin_page_manage_assignment_plugins('assignsubmission'));
-
 $ADMIN->add('assignmentplugins', new admin_category('assignfeedbackplugins',
-        get_string('feedbackplugins', 'assign'), !$module->visible));
-
+        new lang_string('feedbackplugins', 'assign'), !$module->visible));
 $ADMIN->add('assignfeedbackplugins', new admin_page_manage_assignment_plugins('assignfeedback'));
 
 
@@ -46,10 +43,10 @@ assignment_plugin_manager::add_admin_assignment_plugin_settings('assignfeedback'
 foreach (get_plugin_list('assignfeedback') as $type => $notused) {
     $visible = !get_config('assignfeedback_' . $type, 'disabled');
     if ($visible) {
-        $menu['assignfeedback_' . $type] = get_string('pluginname', 'assignfeedback_' . $type);
+        $menu['assignfeedback_' . $type] = new lang_string('pluginname', 'assignfeedback_' . $type);
     }
 }
 
 $settings->add(new admin_setting_configselect('mod_assign_feedback_plugin_for_gradebook', 
-                      get_string('feedbackpluginforgradebook', 'mod_assign'),
-                      get_string('feedbackplugin', 'mod_assign'), 'feedback_comments', $menu));
+                      new lang_string('feedbackpluginforgradebook', 'mod_assign'),
+                      new lang_string('feedbackplugin', 'mod_assign'), 'feedback_comments', $menu));
