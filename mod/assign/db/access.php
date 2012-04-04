8865561ee53b72ab60735b171537fd0361accc6a
Sam Hemelryk <sam@moodle.com>
2012-04-05 09:19:53 +1200

    MDL-31270 mod_assign: Added mod/assign:addinstance capability definition

--- mod/assign/db/access.php
+++ mod/assign/db/access.php
@@ -69,6 +69,18 @@ $capabilities = array(
             'student' => CAP_ALLOW,
         )
     ),
+
+    'mod/assign:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
 );
 
 
