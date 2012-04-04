448c99424490479e62749453f625abf9974e46fa
Raymond Wijaya <raymond.wijaya@netspot.com.au>
2012-04-04 12:16:52 +0800

    MDL-31270: Rewrap all the settings with 'if($ADMIN->fulltree)' in settings.php

--- mod/assign/settings.php
+++ mod/assign/settings.php
@@ -46,7 +46,8 @@ defined('MOODLE_INTERNAL') || die;
             $menu['assignfeedback_' . $type] = new lang_string('pluginname', 'assignfeedback_' . $type);
         }
     }
-
+    if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configselect('mod_assign_feedback_plugin_for_gradebook',
                     new lang_string('feedbackpluginforgradebook', 'mod_assign'),
                     new lang_string('feedbackplugin', 'mod_assign'), 'feedback_comments', $menu));
+    }
