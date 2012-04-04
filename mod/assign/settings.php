1636629d07e15bbf30c79377aef579d7d7f8b610
Raymond Wijaya <raymond.wijaya@netspot.com.au>
2012-04-04 10:33:19 +0800

    MDL-31270: Fix error when accessing 'manage assignment plugins' in settings.php by removing $ADMIN->fulltree

--- mod/assign/settings.php
+++ mod/assign/settings.php
@@ -25,7 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-if ($ADMIN->fulltree) {
     require_once($CFG->dirroot . '/mod/assign/adminlib.php');
 
     $ADMIN->add('modules', new admin_category('assignmentplugins',
@@ -51,4 +50,3 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configselect('mod_assign_feedback_plugin_for_gradebook',
                     new lang_string('feedbackpluginforgradebook', 'mod_assign'),
                     new lang_string('feedbackplugin', 'mod_assign'), 'feedback_comments', $menu));
-}
